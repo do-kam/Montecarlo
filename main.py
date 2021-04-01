66092981be164e23140bab8a590a6244f3d68f45
do-kam <dorothea.kam@gmail.com>
2021-04-01 16:46:03 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+#MonteCarlo
+#to compare results: online calc:https://academo.org/demos/estimating-pi-monte-carlo/
+import random as rd
+import numpy as np
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#n = input('Enter the size of random saples: ')
+#print("Sample size is "+n)
+#n = int(n)
+n=5
+zwischenergebnis= np.zeros(n)
+
+def mc_pi(n):
+    circlepoints = 0
+    squarepoints = 0
+    for i in range (0,n):
+        randx = rd.uniform(0, 1)
+        randy = rd.uniform(0, 1)
+        distance= randx**2 + randy**2
+        if (distance<1):
+            circlepoints +=1
+
+        squarepoints +=1
+
+        pi = 4* circlepoints/ squarepoints
+        #global zwischenergebnis
+        zwischenergebnis[i]=pi
+        mc_pi_stat(zwischenergebnis)
+        print("Zwischenergebnis ",zwischenergebnis[i])
+    print("Final Estimation of Pi=", pi)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def mc_pi_stat(zwischenerg):
+    global mittelwert
+    mittelwert=np.mean(zwischenerg)
+    print(mittelwert)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+mc_pi(n)
+print("Mittelwert ist:",mittelwert)
